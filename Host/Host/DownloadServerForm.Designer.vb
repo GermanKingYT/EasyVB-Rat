1b03ec3f79e222abf26525b99431fafd1e9109ed
Modder112 <hiho-jo@web.de>
2016-03-08 23:19:47 +0100

    DonwloadServer Host Finish!
    Wurde noch nicht getestet!

--- Host/Host/DownloadServerForm.Designer.vb
+++ Host/Host/DownloadServerForm.Designer.vb
@@ -22,13 +22,15 @@ Partial Class DownloadServerForm
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim ListViewItem1 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem(New String() {"test", "downloading ..."}, -1)
+        Dim ListViewItem5 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem(New String() {"test", "downloading ..."}, -1)
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.ListView1 = New System.Windows.Forms.ListView()
         Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Panel1 = New System.Windows.Forms.Panel()
         Me.SuspendLayout()
         '
         'Label1
@@ -55,12 +57,14 @@ Partial Class DownloadServerForm
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(342, 20)
         Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "3317"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'ListView1
         '
         Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2})
         Me.ListView1.GridLines = True
-        Me.ListView1.Items.AddRange(New System.Windows.Forms.ListViewItem() {ListViewItem1})
+        Me.ListView1.Items.AddRange(New System.Windows.Forms.ListViewItem() {ListViewItem5})
         Me.ListView1.Location = New System.Drawing.Point(15, 51)
         Me.ListView1.Name = "ListView1"
         Me.ListView1.Size = New System.Drawing.Size(423, 177)
@@ -78,11 +82,23 @@ Partial Class DownloadServerForm
         Me.ColumnHeader2.Text = "Status"
         Me.ColumnHeader2.Width = 96
         '
+        'BackgroundWorker1
+        '
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Red
+        Me.Panel1.Location = New System.Drawing.Point(434, -1)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(17, 17)
+        Me.Panel1.TabIndex = 4
+        '
         'DownloadServerForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(450, 240)
+        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.ListView1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
@@ -99,4 +115,6 @@ Partial Class DownloadServerForm
     Friend WithEvents ListView1 As System.Windows.Forms.ListView
     Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
     Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
 End Class
--- Host/Host/DownloadServerForm.vb
+++ Host/Host/DownloadServerForm.vb
@@ -1,6 +1,77 @@
-﻿Public Class DownloadServerForm
+﻿Imports System.Net.Sockets
+Imports System.Threading
+Imports System.IO
+Imports System.Text
+
+Public Class DownloadServerForm
 
     Private Sub DownloadServer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Not TextBox1.Text = "" Then
+            ChangePanelColor(Color.Green)
+            BackgroundWorker1.RunWorkerAsync()
+        End If
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Try
+            Dim port As Integer = CInt(TextBox1.Text)
+            Dim l As New TcpListener(port)
+            While True
+                Dim client As TcpClient = l.AcceptTcpClient()
+                Dim t As New Thread(AddressOf DownloadFilesClient)
+                t.Start(client)
+            End While
+        Catch ex As Exception
+            MsgBox("Downloadserver are crashed!", MsgBoxStyle.Critical, "ERROR")
+            Me.Invoke(New ChangePanelColor_d(AddressOf ChangePanelColor), Color.Red)
+        End Try
+    End Sub
+
+    Public Sub DownloadFilesClient(ByVal client As TcpClient)
+        Dim filenamedia As New SaveFileDialog()
+        Dim filename As String = ""
+        While True
+            If filenamedia.ShowDialog() = Windows.Forms.DialogResult.OK Then
+                Exit While
+            End If
+        End While
+        filename = filenamedia.FileName
+        Try
+            Dim lvi As ListViewItem = Me.Invoke(New AddListBoxItem_d(AddressOf AddListBoxItem), filename)
+            Dim networkStream As NetworkStream = client.GetStream()
+            Dim bytes(client.ReceiveBufferSize) As Byte
+            networkStream.Read(bytes, 0, CInt(client.ReceiveBufferSize))
+            Dim clientdata As String = Encoding.ASCII.GetString(bytes)
+            Dim fi As New FileInfo(filename)
+            Dim sw As StreamWriter = fi.CreateText()
+            sw.Write(clientdata)
+            sw.Close()
+            Me.Invoke(New ChangeListBoxItem_d(AddressOf ChangeListBoxItem), lvi)
+        Catch e As Exception
+
+        End Try
+    End Sub
+
+    Public Delegate Function AddListBoxItem_d(ByVal filename As String) As ListViewItem
+    Public Function AddListBoxItem(ByVal filename As String)
+        Dim f As New IO.FileInfo(filename)
+        Dim listviewitem As New ListViewItem(f.Name)
+        listviewitem.SubItems.Add("downloading ...")
+        Return ListView1.Items.Add(listviewitem)
+    End Function
+
+    Public Delegate Sub ChangeListBoxItem_d(ByVal filename As ListViewItem)
+    Public Sub ChangeListBoxItem(ByVal filename As ListViewItem)
+        filename.SubItems(0).Text = "finish"
+    End Sub
+
+    Public Delegate Sub ChangePanelColor_d(ByVal color As Color)
+    Public Sub ChangePanelColor(ByVal color As Color)
+        Panel1.BackColor = color
+    End Sub
+
 End Class
